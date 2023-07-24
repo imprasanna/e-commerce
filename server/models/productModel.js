8cd270ca8e13ad44684ed2371ddcbbf7aed03d19
Prasanna Acharya <prasannaacharya2073@gmail.com>
2023-07-24 17:43:24 +0545

    Schema bug fix

--- server/models/productModel.js
+++ server/models/productModel.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
-// const Schema = mongoose.Schema;
+const Schema = mongoose.Schema;
 
-const productSchema = mongoose.Schema({
+const productSchema = new Schema({
   name: {
     type: String,
     required: [true, "Please enter product name"],
