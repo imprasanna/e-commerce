76b7d0b613e6a2ba7b8a6574d6c0349ffe2f6cc2
Prasanna Acharya <prasannaacharya2073@gmail.com>
2023-07-25 08:44:30 +0545

    Some other mongoose functions or database functionalities added

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -19,8 +19,71 @@ const createProduct = async (req, res, next) => {
   }
 };
 
-const getAllProducts = (req, res) => {
-  res.status(200).json({ message: "Route is working well" });
+const getAllProducts = async (req, res) => {
+  const products = await Product.find();
+
+  res.status(200).json({
+    success: true,
+    products,
+  });
 };
 
-module.exports = { getAllProducts, createProduct };
+// Update products for admin
+const updateProduct = async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res
+      .status(500)
+      .json({ success: false, message: "Product not found" });
+  }
+
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+  module.exports = { getAllProducts, createProduct, updateProduct };
+
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
+
+const getProductDetails = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res
+      .status(500)
+      .json({ success: false, message: "Product not found" });
+  }
+
+  res.status(200).json({ success: true, product });
+};
+
+const deleteProduct = async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res
+      .status(500)
+      .json({ success: false, message: "Product not found" });
+  }
+
+  await product.deleteOne();
+
+  res.status(200).json({
+    success: true,
+    message: "Product deleted successfully",
+  });
+};
+
+module.exports = {
+  getAllProducts,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  getProductDetails,
+};
--- server/routes/productRoute.js
+++ server/routes/productRoute.js
@@ -1,11 +1,17 @@
 const {
   getAllProducts,
   createProduct,
+  updateProduct,
+  deleteProduct,
+  getProductDetails,
 } = require("../controllers/productController");
 
 const productRoute = (app) => {
   app.get("/api/products", getAllProducts);
   app.post("/api/product/new", createProduct);
+  app.put("/api/product/:id", updateProduct);
+  app.get("/api/product/:id", getProductDetails);
+  app.delete("/api/product/:id", deleteProduct);
 };
 
 module.exports = { productRoute };
