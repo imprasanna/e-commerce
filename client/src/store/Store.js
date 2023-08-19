d630f725715afcc49f2d338e89e43c2e1ce977fd
Prasanna Acharya <prasannaacharya2073@gmail.com>
2023-08-19 14:15:07 +0545

    Global states for mobile bottom nav created in redux-tk store

--- client/src/store/Store.js
+++ client/src/store/Store.js
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import navSlice from "./slices/NavSlice";
+import mobNavSlice from "./slices/MobNavSlice";
 
 const store = configureStore({
-  reducer: { nav: navSlice },
+  reducer: {
+    nav: navSlice,
+    mobNav: mobNavSlice,
+  },
 });
 
 export default store;
