32173fa62695bd608b156350a26a6865c8dd6d76
Prasanna Acharya <prasannaacharya2073@gmail.com>
2023-08-22 06:01:46 +0545

    Search box created in search section for mobile-view

--- client/src/components/MobSearch.js
+++ client/src/components/MobSearch.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { motion } from "framer-motion";
 import { useSelector } from "react-redux";
+import { AiOutlineSearch } from "react-icons/ai";
 import "../styles/mobsearch.css";
 
 const MobSearch = () => {
@@ -33,7 +34,15 @@ const MobSearch = () => {
           variants={variants}
           className="mobile-search bg-[#000000b3] fixed top-0 bottom-0 right-0 left-0 z-50 text-white"
         >
-          <div className="w-full mt-[6rem]">Search</div>
+          <div className="search-box-wrapper w-full mt-[6rem] flex justify-center">
+            <div className="search-box w-[250px] xxs:w-[300px] xs:w-[350px] sm: md1:w-[450px] md:w-[550px] md2:w-[600px] bg-white flex items-center rounded-3xl pl-2">
+              <AiOutlineSearch className="text-black h-[2rem]" />
+              <input
+                className="block w-[200px] xxs:w-[250px] xs:w-[300px] md1:w-[400px] md:w-[500px] md2:w-[550px] h-[40px] border-0 pl-4 text-[#959595] font-ysabeau focus:outline-0 placeholder:font-ysabeau placeholder:text-[0.9rem] placeholder:text-[#e3e3e3]"
+                placeholder="Search for products"
+              />
+            </div>
+          </div>
         </motion.div>
       </div>
     </div>
