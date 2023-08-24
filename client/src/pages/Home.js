3bf6122df97d732bf253587dc77cc760bf00948b
Prasanna Acharya <prasannaacharya2073@gmail.com>
2023-08-24 20:22:55 +0545

    A new products page created

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -9,6 +9,7 @@ import {
   hideCrossIcon,
   setMenuOpen,
 } from "../store/slices/NavSlice";
+import { RiArrowDropDownLine } from "react-icons/ri";
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -33,7 +34,65 @@ const Home = () => {
         <Nav3 />
         <div className="body-wrapper">
           <div className={dark === true ? "dark-banner" : "banner"}></div>
-          <div></div>
+          <div className="body">
+            <p
+              style={{ textAlign: "center" }}
+              className="text-2xl mt-8 mb-3 md:text-3xl lg:text-4xl lg:mt-16 lg:mb-8"
+            >
+              Our <strong>New Products</strong>
+            </p>
+            <div className="w-full border-t-[1px] border-b-[1px] border-[#e0e0e0] flex mb-4">
+              <div className="w-[70%]"></div>
+              <div className="w-[30%] border-l-[1px] border-[#e0e0e0] p-3 flex items-center justify-center">
+                <div>Filters</div>
+                <RiArrowDropDownLine />
+              </div>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Suspendisse nec libero et ante euismod elementum et suscipit
+              lorem. Etiam non scelerisque lectus, quis hendrerit lorem. Mauris
+              ultrices pulvinar augue quis tristique. Pellentesque et ligula
+              sapien. Nunc sit amet massa gravida, vehicula metus sit amet,
+              commodo sapien. Nunc euismod magna et quam efficitur varius.
+              Mauris mattis enim at ante luctus tincidunt. Suspendisse nec
+              ligula euismod, ultricies erat at, posuere felis. Integer vitae
+              elit et nibh semper tincidunt. Nunc vehicula rhoncus tellus, porta
+              mattis libero lobortis ut. Aenean malesuada cursus aliquet.
+              Pellentesque tempor est sit amet semper tincidunt. In ac varius
+              elit. Morbi feugiat non tortor condimentum venenatis. Quisque
+              ligula quam, efficitur vitae elit ac, iaculis cursus ligula.
+              Aenean efficitur dui vel tortor suscipit congue. Nam leo elit,
+              ultrices non dapibus mollis, volutpat ut sem. Integer nec congue
+              felis. Class aptent taciti sociosqu ad litora torquent per conubia
+              nostra, per inceptos himenaeos. Donec feugiat, ex ac interdum
+              ullamcorper, leo lacus hendrerit massa, sit amet auctor eros
+              mauris sed velit. Nulla maximus, nulla id facilisis faucibus,
+              turpis dui dictum eros, quis pretium lorem enim et felis. Integer
+              vel sollicitudin massa. Suspendisse aliquet erat vitae ante
+              congue, eu tincidunt arcu tempor. Aliquam eget mi lobortis,
+              vulputate justo at, efficitur purus. Quisque sollicitudin urna sed
+              magna egestas lobortis. Vivamus vitae commodo odio. Fusce id
+              lacinia leo. Ut feugiat, quam sit amet pharetra ultrices, quam
+              libero viverra nulla, vitae ullamcorper sem dolor eget augue.
+              Praesent placerat velit ut lectus commodo vehicula. Pellentesque
+              in lorem nibh. Sed at aliquet lectus, sed porttitor orci. Sed at
+              tempus nibh, vel molestie arcu. Integer nibh neque, venenatis et
+              libero a, lacinia imperdiet dui. Nullam porta lacus ut justo
+              fringilla fringilla. Pellentesque mollis metus ac lobortis
+              dignissim. Vivamus suscipit ligula massa. Nunc id dolor libero. In
+              hac habitasse platea dictumst. Integer sed leo maximus, cursus
+              libero hendrerit, luctus felis. Phasellus molestie luctus justo et
+              condimentum. Integer condimentum libero a ligula auctor, vel
+              scelerisque magna sagittis. Aliquam eget ullamcorper justo. Donec
+              blandit arcu in arcu facilisis dapibus. Cras libero sapien,
+              pharetra vehicula arcu nec, tincidunt fringilla lectus. Donec id
+              vestibulum dolor. Ut rutrum vel velit non sollicitudin. Orci
+              varius natoque penatibus et magnis dis parturient montes, nascetur
+              ridiculus mus. Donec finibus turpis nec augue ultrices egestas.
+              Nam arcu mi, luctus sed laoreet sed, scelerisque id orci.
+            </p>
+          </div>
         </div>
       </div>
     </>
---
+++ client/src/pages/Products.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Products = () => {
+  return (
+    <div>
+      <p>
+        Explore a wide range of our products with exciting offers.
+        <br />
+        <strong>Shop now!</strong>
+      </p>
+    </div>
+  );
+};
+
+export default Products;
