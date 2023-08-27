d806caec871448b2de9514f0017cd5d4c3d96182
Prasanna Acharya <prasannaacharya2073@gmail.com>
2023-08-27 14:21:51 +0545

    Products page optimized and maintained

--- client/src/pages/Products.js
+++ client/src/pages/Products.js
@@ -1,10 +1,19 @@
 import React from "react";
+import Nav1 from "../components/Nav1";
+import Nav2 from "../components/Nav2";
+import Nav3 from "../components/Nav3";
 import { RiArrowDropDownLine } from "react-icons/ri";
 import "../styles/products.css";
-// #fbdce2 ffd1d9
+import products from "../utils/products";
+import ProductCardDetails from "../components/ProductCardDetails";
+import { Grid } from "@mui/material";
+
 const Products = () => {
   return (
     <div>
+      <Nav1 />
+      <Nav2 />
+      <Nav3 />
       <p
         className="top-banner p-4 text-lg sm:text-2xl sm:p-8"
         style={{
@@ -24,6 +33,32 @@ const Products = () => {
           <RiArrowDropDownLine />
         </div>
       </div>
+
+      <p className="lg:w-[80%] ml-auto mr-auto">
+        <Grid container className="lg:w-[80%] ml-auto mr-auto">
+          {products.map((product) => {
+            return (
+              <Grid
+                xs={12}
+                sm={6}
+                md={4}
+                lg={3}
+                rowSpacing={2}
+                key={product.id}
+              >
+                <div key={product.id}>
+                  <ProductCardDetails
+                    id={product.id}
+                    name={product.name}
+                    image={product.img}
+                    price={product.price}
+                  />
+                </div>
+              </Grid>
+            );
+          })}
+        </Grid>
+      </p>
     </div>
   );
 };
